bf872205ccda56fbcaedfe5cea2a443f22a4bf13
xiongmeijuan <hzxmj@tairanchina.com>
2018-02-28 14:10:02 +0800

    update

---
+++ src/routes/login/index.js
@@ -0,0 +1,87 @@
+import React, { PropTypes } from 'react';
+import { Button, Row, Form, Input } from 'antd';
+import { config } from '../../utils';
+import styles from './index.less';
+
+const FormItem = Form.Item;
+
+const login = ({
+  loginButtonLoading,
+  onOk,
+  form: { getFieldDecorator, validateFieldsAndScroll }
+}) => {
+  function handleOk() {
+    validateFieldsAndScroll((errors, values) => {
+      if (errors) {
+        return;
+      }
+      onOk(values);
+    });
+  }
+  return (
+    <div>
+      <div className={styles.form}>
+        <div className={styles.logo}>
+          <img alt={'logo'} src={config.logoSrc} />
+          <span>RestAPI Module</span>
+        </div>
+        <form>
+          {/* hasFeedback：用于给输入框添加反馈图标 */}
+          <FormItem hasFeedback>
+            {
+              getFieldDecorator('name', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请填写用户名',
+                  },
+                ],
+              })(
+                <Input
+                  size="large"
+                  onPressEnter={handleOk}
+                  placeholder="用户名"
+                />,
+              )
+            }
+          </FormItem>
+          <FormItem hasFeedback>
+            {
+              getFieldDecorator('pass', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请填写密码',
+                  },
+                ],
+              })(
+                <Input
+                  size="large"
+                  type="password"
+                  onPressEnter={handleOk}
+                  placeholder="密码"
+                />,
+            )}
+          </FormItem>
+          <Row>
+            <Button
+              type="primary"
+              size="large"
+              onClick={handleOk}
+              loading={loginButtonLoading}
+            >
+              登录
+            </Button>
+          </Row>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+login.propTypes = {
+  form: PropTypes.object,
+  loginButtonLoading: PropTypes.bool,
+  onOk: PropTypes.func,
+};
+export default FormItem.create()(login);
